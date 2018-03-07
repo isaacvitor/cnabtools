5a7a1acf2cc63ecf746444954ee6a33d9ffda9ec
Isaac Vitor <isaacvitor@gmail.com>
2018-03-07 16:18:09 -0300

    remove logs

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const layoutHelpers = require('./util/layout_helper')
 // Load banks
 let banks = {};
 const banksFolders = fs.readdirSync(__dirname + '/banks/');
-console.log('banksFolders', banksFolders);
 
 for (var i = 0; i < banksFolders.length; i++) {
     banks[banksFolders[i]] = require(__dirname + '/banks/' + banksFolders[i] + '/index.js');
@@ -12,5 +11,3 @@ for (var i = 0; i < banksFolders.length; i++) {
 
 exports.banks = banks
 exports.layoutHelpers = layoutHelpers
-
-console.log(banks['001'].layouts.remessa)
